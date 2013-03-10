78fb15690025cb68baedcb92045bbe93f602e043
JimmyCDChen <chundachen1230@gmail.com>
2013-03-10 17:07:08 -0400

    Added the producer and customer function, ready to getting information

--- cdcphotoalbum/main.py
+++ cdcphotoalbum/main.py
@@ -1,230 +1,101 @@
-#!/usr/bin/env python
 import cgi, cgitb
-import datetime
 import urllib
 import webapp2
 import jinja2
 import os
 import thread
-import re
-from django.shortcuts import render_to_response
-from xml.etree import ElementTree
-
 from google.appengine.ext import db
 from google.appengine.api import users
+import gdata.photos.service
+import gdata.media
+import gdata.geo
+import feedparser
+
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
     
-class Categories(db.Model):
+
+class Photo(db.Model):
+  id = db.IntegerProperty()
   author = db.StringProperty()
-  category = db.StringProperty()
+  title = db.StringProperty()
 
-# Insert or edit a new item into a category
-class Category(webapp2.RequestHandler):
-  def post(self):
 
-    user = users.get_current_user().nickname()
-    submit = self.request.get('submit')
-    newcat = self.request.get('content')
-    response =""
-    
-    if submit == "Add":
-      checkcategory = Categories.all().filter("category =", newcat).get()
-      if checkcategory is None:
-        newcategory = Categories()
-        newcategory.author = self.request.get('user')
-        newcategory.category = self.request.get('content')
-        newcategory.put()
-      else:
-        response = "This category name is used, please choose another name."
-        
-    elif submit == "Delete":
-      deletename = self.request.get('deletename')
-      checkcategory = Categories.all().filter("category =", deletename).filter("author =", user).get()
-      checkitem = Item.all().filter("categories =", deletename).filter("author =", user)
-      if checkcategory is None:
-        # The old name doesn't exist in the database
-        response = "This item does not exist. Plese enter a valid item name to delete."
-      else:
-        if checkcategory.author == user:
-          db.delete(checkcategory)
-          db.delete(checkitem)
-          response = "You have successfully delete an item."
-        else:
-          response = "This item is not owned by you, no changes made"  
-    
-    self.redirect('/?' + urllib.urlencode({'categories': self.request.get('content')}) + urllib.urlencode({'response': response}))
-  
-class Item(db.Model):
-  author = db.StringProperty()
-  categories = db.StringProperty()
-  item = db.StringProperty()
-  win = db.IntegerProperty()
-  lose = db.IntegerProperty()
-  percentage = db.StringProperty()
+class Retrieve(webapp2.RequestHandler):
+    QUEUE = []
+    def get(self):
+        user = users.get_current_user()
+        photoAlbum = self.request.get('albumName')
+        gd_client = gdata.photos.service.PhotosService()
+        albums = gd_client.GetUserFeed(user=user)
 
-def percentage(part, whole):
-  return 100 * float(part)/float(whole)
-  
-# Insert or edit a new item into a category
-class Itembook(webapp2.RequestHandler):
-  def get(self):
 
-    user = users.get_current_user().nickname()
-    category = self.request.get('category')
-    item_input = self.request.get('content')
-    choice = self.request.get('choice')
-    item1_old = self.request.get('item1')
-    item2_old = self.request.get('item2')   
-    greeting =""
-    submit = self.request.get('submit')
-    
-    # get category owner
-    catowner = Categories.all().filter("category =", category).get()
-    
-    if submit == "Vote":
-      # edit the voting result
-      if choice == "1":
-        # choose 1st choice
-        item = Item.all().filter("categories =", category).filter("item =", item1_old).get()
-        item.win = item.win +1
-        item.percentage = "{0:.0f}%".format(percentage(item.win, (item.win + item.lose)))
-        item.put()
-        
-        item = Item.all().filter("categories =", category).filter("item =", item2_old).get()
-        item.lose = item.lose +1
-        item.percentage = "{0:.0f}%".format(percentage(item.win, (item.win + item.lose)))
-        item.put()
-        
-        greeting = "You choose %s over %s" % (item1_old, item2_old)
-      elif choice == "2":
-        # choose 2nd choice
-        item = Item.all().filter("categories =", category).filter("item =", item2_old).get()
-        item.win = item.win +1
-        item.percentage = "{0:.0f}%".format(percentage(item.win, (item.win + item.lose)))
-        item.put()
-        
-        item = Item.all().filter("categories =", category).filter("item =", item1_old).get()
-        item.lose = item.lose +1
-        item.percentage = "{0:.0f}%".format(percentage(item.win, (item.win + item.lose)))
-        item.put()
-        greeting = "You choose %s over %s" % (item2_old, item1_old)
-    
-    if catowner is None or user == catowner.author:
-      if submit == "Add":        
-          # New category with items to add
-          if item_input != "" :
-              checkitem = Item.all().filter("categories =", category).filter("item =", item_input).get()
-              if checkitem is None:
-                  # add new item into category
-                  item = Item()
-                  item.author = users.get_current_user().nickname()
-                  item.categories = category
-                  item.item = item_input
-                  item.win = 0
-                  item.lose = 0
-                  item.percentage = "-"
-                  item.put()
-                  greeting = "Item %s is added into Category %s" % (item_input, category)
-              else:
-                  greeting = "This item exists in the category already."
-      elif submit == "Edit":
-          oldname = self.request.get('oldname')
-          newname = self.request.get('newname')
-          checkitem = Item.all().filter("categories =", category).filter("author =", user).filter("item =", oldname).get()
-          if checkitem is None:
-              # The old name doesn't exist in the database
-              greeting = "This item does not exist. Plese enter a valid item name to change."
-          else:
-              if checkitem.author == user:
-                  checkitem.item = newname
-                  checkitem.put()
-                  greeting = "You have successfully edit an item."
-              else:
-                  greeting = "This item is not owned by you, no changes made"
-      elif submit == "Delete":
-          deletename = self.request.get('deletename')
-          checkitem = Item.all().filter("categories =", category).filter("author =", user).filter("item =", deletename).get()
-          if checkitem is None:
-              # The old name doesn't exist in the database
-              greeting = "This item does not exist. Plese enter a valid item name to delete."
-          else:
-              if checkitem.author == user:
-                  db.delete(checkitem)
-                  greeting = "You have successfully delete an item."
-              else:
-                  greeting = "This item is not owned by you, no changes made"
-      elif submit == "DeleteALL":
-          checkitem = Item.all().filter("categories =", category).filter("author =", user)
-          test = checkitem.get()
-          if test is None:
-              # The old name doesn't exist in the database
-              greeting = "No item exists."
-          else:
-              if test.author == user:
-                  db.delete(checkitem)
-                  greeting = "You have successfully made change."
-              else:
-                  greeting = "This item is not owned by you, no changes made"
-    else:
-      greeting ="Sorry, only the owner of this category can make changes."
-      
-    # For voting purposes
-    itemlist = Item.all().filter("categories =", category).order("-win")
-    itemlist_total = itemlist.count()
-    if itemlist_total >= 2:
-        one = random.randrange(0, itemlist_total)
-        two = random.randrange(0, itemlist_total)
-        while one == two:
-            two = random.randrange(0, itemlist_total)
-        item_one = itemlist[one]
-        item_two = itemlist[two]        
-    else:
-        item_one = "NULL"
-        item_two = "NULL"        
-        
-    template_values = {
-        'user': user,
-        'owner': catowner.author,
-        'category': category,
-        'item_one': item_one,
-        'item_two': item_two,
-        'greeting': greeting,
-        'itemlist': itemlist,
-    }
-        
-    template = jinja_environment.get_template('item.html')
-    self.response.out.write(template.render(template_values))
+	# this is executed in one threading.Thread object
+    def producer():
+        global QUEUE
+        while True:
+            i = produce_item()
+            QUEUE.append(i)
 
-class Item(db.Model):
-  author = db.StringProperty()
-  categories = db.StringProperty()
-  item = db.StringProperty()
-  win = db.IntegerProperty()
-  lose = db.IntegerProperty()
-  percentage = db.StringProperty()
-  date = db.DateTimeProperty(auto_now_add=True)
+        gd_client = gdata.photos.service.PhotosService()
 
-def percentage(part, whole):
-  return 100 * float(part)/float(whole)
+	# this is executed in another threading.Thread object
+    def consumer():
+        global QUEUE
+        while True:
+            try:
+                i = QUEUE.pop(0)
+            except IndexError:
+                # queue is empty
+                continue
+
+            consume_item(i)
+
+
+# Function to retrive the user's public album list
+def getAlbumList(userName):
+        gd_client = gdata.photos.service.PhotosService()
+        albumList = gd_client.GetUserFeed(user=userName)
+        albumInfo = []
+        for album in albumList:
+            albumInfo.append(album.gphoto_id.text)
+            albumInfo.append(album.title.text)
+            albumInfo.append(album.numphotos.text)
+
+        return albumInfo
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        # verify if the user is logged in or logged out
-        submit = self.request.get('submit')
+
+    	user = users.get_current_user()
+
+        if user is not None:
+            logInOut = users.create_logout_url(self.request.uri)
+            greeting = "Please Enter Your Picasa Album Address:"
+            albumsInfo = Retrieve(user)
+            print albumsInfo
+        else:
+    
+            logInOut = users.create_login_url(self.request.uri)
+            greeting = "Please Login to continue."
+                                           
+        template_values = {
+        		'user': user,
+        		'logInOut': logInOut,
+                'greeting': greeting,
+                'albumsInfo': albumsInfo,
+        }
         
-        response = self.request.get('response')
-        greeting = "Please enter the album url to begin."
-        noAlbum = True
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))
 
+#authSubUrl = GetAuthSubUrl();
+#print '<a href="%s">Login to your Google account</a>' % authSubUrl
 
-        return render_to_response(request, 'index.html', locals())
-                                   
-#        template = jinja_environment.get_template('index.html')
-#        self.response.out.write(template.render(template_values))
             
     
-app = webapp2.WSGIApplication([('/', MainPage)],
+app = webapp2.WSGIApplication([('/', MainPage),
+                                ('/Retrieve', Retrieve)],
                                debug=True)
                                
