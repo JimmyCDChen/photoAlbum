2ad21e67db7046ddbf4da23c569bb93336b0933e
JimmyCDChen <chundachen1230@gmail.com>
2013-03-10 22:59:52 -0400

    successful retrieve information with single thread operation

--- cdcphotoalbum/main.py
+++ cdcphotoalbum/main.py
@@ -4,32 +4,98 @@ import webapp2
 import jinja2
 import os
 import thread
+import datetime
 from google.appengine.ext import db
 from google.appengine.api import users
 import gdata.photos.service
 import gdata.media
 import gdata.geo
-import feedparser
 
+albumRef = {}
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
     
-
 class Photo(db.Model):
-  id = db.IntegerProperty()
-  author = db.StringProperty()
-  title = db.StringProperty()
+    photoid = db.StringProperty()
+    author = db.StringProperty()
+    title = db.StringProperty()
+    time = db.DateTimeProperty(auto_now_add=True)
+    exposure = db.StringProperty()
+    focallength = db.StringProperty()
+    make = db.StringProperty()
+    fstop = db.StringProperty()
+    model = db.StringProperty()
+    flash = db.StringProperty()
+    iso = db.StringProperty()
+    thumbnail = db.StringProperty()
 
 
 class Retrieve(webapp2.RequestHandler):
     QUEUE = []
     def get(self):
         user = users.get_current_user()
-        photoAlbum = self.request.get('albumName')
+        print albumRef
+        albumName = self.request.get('albumName')
+        photoAlbumID = albumRef[albumName]
         gd_client = gdata.photos.service.PhotosService()
-        albums = gd_client.GetUserFeed(user=user)
+        photoList = gd_client.GetFeed('/data/feed/api/user/%s/albumid/%s?kind=photo' % (user, photoAlbumID))
 
+        # Clear all temporary photo data stored in the db
+        photoAll = Photo.all()
+        db.delete(photoAll)
+
+        for photo in photoList.entry:
+            newPhoto = Photo()
+            newPhoto.photoid = photo.gphoto_id.text
+            newPhoto.thumbnail = photo.media.thumbnail[2].url
+            newPhoto.author = photo.media.credit.text
+            newPhoto.title = photo.title.text
+            try:
+                newPhoto.time = datetime.datetime.fromtimestamp(float(photo.exif.time.text) / 1e3)
+            except:
+                pass
+            try:
+                newPhoto.exposure = photo.exif.exposure.text
+            except:
+                pass
+            try:
+                newPhoto.focallength = photo.exif.focallength.text
+            except:
+                pass
+            try:
+                newPhoto.make = photo.exif.make.text
+            except:
+                pass
+            try:
+                newPhoto.fstop = photo.exif.fstop.text
+            except:
+                pass
+            try:
+                newPhoto.model = photo.exif.model.text
+            except:
+                pass
+            try:
+                newPhoto.flash = photo.exif.flash.text
+            except:
+                pass
+            try:
+                newPhoto.iso = photo.exif.iso.text
+            except:
+                pass
+
+            newPhoto.put()
+        
+        allPhoto = Photo.all()
+
+        template_values = {
+                'user': user,
+                'allPhoto': allPhoto,
+                'albumName': self.request.get('albumName'),
+        }
+        
+        template = jinja_environment.get_template('photo.html')
+        self.response.out.write(template.render(template_values))
 
 	# this is executed in one threading.Thread object
     def producer():
@@ -53,28 +119,25 @@ class Retrieve(webapp2.RequestHandler):
             consume_item(i)
 
 
-# Function to retrive the user's public album list
-def getAlbumList(userName):
-        gd_client = gdata.photos.service.PhotosService()
-        albumList = gd_client.GetUserFeed(user=userName)
-        albumInfo = []
-        for album in albumList:
-            albumInfo.append(album.gphoto_id.text)
-            albumInfo.append(album.title.text)
-            albumInfo.append(album.numphotos.text)
-
-        return albumInfo
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
 
     	user = users.get_current_user()
-
+        albumID = []
+        albumTitle = []
+        albumPhotoNum = []
         if user is not None:
             logInOut = users.create_logout_url(self.request.uri)
             greeting = "Please Enter Your Picasa Album Address:"
-            albumsInfo = Retrieve(user)
-            print albumsInfo
+            gd_client = gdata.photos.service.PhotosService()
+            albumList = gd_client.GetUserFeed(user=user)
+
+            for album in albumList.entry:
+                albumID.append(album.gphoto_id.text)
+                albumRef[album.title.text] = album.gphoto_id.text
+                albumTitle.append(album.title.text)
+                albumPhotoNum.append(album.numphotos.text)
+
         else:
     
             logInOut = users.create_login_url(self.request.uri)
@@ -84,16 +147,11 @@ class MainPage(webapp2.RequestHandler):
         		'user': user,
         		'logInOut': logInOut,
                 'greeting': greeting,
-                'albumsInfo': albumsInfo,
+                'albumInfo': zip(*(albumTitle, albumID, albumPhotoNum)),
         }
         
         template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render(template_values))
-
-#authSubUrl = GetAuthSubUrl();
-#print '<a href="%s">Login to your Google account</a>' % authSubUrl
-
-            
     
 app = webapp2.WSGIApplication([('/', MainPage),
                                 ('/Retrieve', Retrieve)],
