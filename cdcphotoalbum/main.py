748964eec78603e414a611391a1efa19ce296d29
JimmyCDChen <chundachen1230@gmail.com>
2013-03-23 01:07:37 -0400

    convert into better project structure

--- cdcphotoalbum/main.py
+++ cdcphotoalbum/main.py
@@ -2,37 +2,18 @@ import cgi, cgitb
 import urllib
 import webapp2
 import jinja2
-import os, sys
-import datetime
+import os, time, datetime
+from models import Album, Photo
+from tool import render_template, writeToDB
 from Queue import Queue
 from threading import Thread, Lock
-import time
 from google.appengine.ext import db
 from google.appengine.api import users
 import gdata.photos.service
 import gdata.media
 
-albumRef = {}
-albumPhoto = {}
 numProducer = 1
 numConsumer = 1
-
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-    
-class Photo(db.Model):
-    photoid = db.StringProperty()
-    author = db.StringProperty()
-    title = db.StringProperty()
-    time = db.DateTimeProperty(auto_now_add=True)
-    exposure = db.StringProperty()
-    focallength = db.StringProperty()
-    make = db.StringProperty()
-    fstop = db.StringProperty()
-    model = db.StringProperty()
-    flash = db.StringProperty()
-    iso = db.StringProperty()
-    thumbnail = db.StringProperty()
 
 #def producer(threadID, albumID):
 #    global photoQueue
@@ -42,63 +23,17 @@ class Photo(db.Model):
 #
 #    for photo in photoList.entry:
 ##        with stdout_mutex:
- #           print "111"
  #           photoQueue.put(photo)
  #           time.sleep(0.1)
 
-photoQueue = Queue()
-
-           
 # this is executed in another threading.Thread object
-def consumer(queue, numPhoto, lock):
-#    global photoQueue
+def consumer(queue, numPhoto, nickname, lock):
+#    metadata = [exposure, focallength, make, fstop, model, flash, iso]
     lock.acquire()
     for i in range(int(numPhoto)):
-        print "I'm trying", i
         photo = queue.get()
-
-        newPhoto = Photo()
-        newPhoto.photoid = photo.gphoto_id.text
-        newPhoto.thumbnail = photo.media.thumbnail[2].url
-        newPhoto.author = photo.media.credit.text
-        newPhoto.title = photo.title.text
-        try:
-            newPhoto.time = datetime.datetime.fromtimestamp(float(photo.exif.time.text) / 1e3)
-        except:
-            pass
-        try:
-            newPhoto.exposure = photo.exif.exposure.text
-        except:
-            pass
-        try:
-            newPhoto.focallength = photo.exif.focallength.text
-        except:
-            pass
-        try:
-            newPhoto.make = photo.exif.make.text
-        except:
-            pass
-        try:
-            newPhoto.fstop = photo.exif.fstop.text
-        except:
-            pass
-        try:
-            newPhoto.model = photo.exif.model.text
-        except:
-            pass
-        try:
-            newPhoto.flash = photo.exif.flash.text
-        except:
-            pass
-        try:
-            newPhoto.iso = photo.exif.iso.text
-        except:
-            pass
-
-        # Add to db after retrieved
-        newPhoto.put()
-        print "a photo has been added to Queue"
-        photoQueue.task_done()
+        writeToDB(photo, nickname)
+        queue.task_done()
 
     # release when all photo are added into db        
     lock.release()
@@ -106,47 +41,51 @@ def consumer(queue, numPhoto, lock):
 class Retrieve(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        albumName = self.request.get('albumName')
-        photoAlbumID = albumRef[albumName]
-        numPhoto = albumPhoto[albumName]
-
-#        joinable = []
-#        # Call producer to retrieve photos
-#        for i in range(numProducer):
-#            t = threading.Thread(target=producer, args=(i, photoAlbumID))
-#            joinable.append(t)
-#            t.start()
-
+        albumName = self.request.get('name')
+        nickname = users.get_current_user().nickname()
+        
         # Clear all photo data stored in the db
-        photoAll = Photo.all()
-        db.delete(photoAll)
+        query = "Where owner = '%s'" % (user)
+        allPhoto = Photo.gql(query)
+        db.delete(allPhoto)
+        # Get selected album with owner
+        query = "Where albumName = '%s' AND owner ='%s'" % (albumName, nickname)
+        selectedAlbum = Album.gql(query)
+        # Pre-select if album is empty
+        if selectedAlbum[0].albumPhoto == '0':
+            template_values = {
+                'user': user,
+                'albumName': albumName,
+            }
+            render_template(self, 'photo.html', template_values)
 
         gd_client = gdata.photos.service.PhotosService()
-        photoList = gd_client.GetFeed('/data/feed/api/user/%s/albumid/%s?kind=photo' % (user, photoAlbumID))
+        photoList = gd_client.GetFeed('/data/feed/api/user/%s/albumid/%s?kind=photo' % (user, selectedAlbum[0].albumID))
 
-
+        # initialize Lock and Queue objects
         lock = Lock()
+        photoQueue = Queue()
 
         for photo in photoList.entry:
             photoQueue.put(photo)
 
         # start consumer
         for i in range(numConsumer):
-            worker = Thread(target=consumer, args=(photoQueue, numPhoto, lock))
+            worker = Thread(target=consumer, args=(photoQueue, selectedAlbum[0].albumPhoto, nickname, lock))
             worker.start()
 
 #        for thread in joinable:
 #            thread.join()
         photoQueue.join()
 
-        sys.stdout.write("All photo has been added into Queue.\n")
         # All consumer thread are finished, empty photoQueue
 #        with stdout_mutex:
         print "All done"
 
         lock.acquire()
-        time.sleep(0.5)
-        allPhoto = Photo.all()
+        time.sleep(1)
+        query = "Where owner = '%s'" % (nickname)
+        allPhoto = Photo.gql(query)
         print allPhoto.count()
 
         template_values = {
@@ -154,48 +93,71 @@ class Retrieve(webapp2.RequestHandler):
                 'allPhoto': allPhoto,
                 'albumName': albumName,
         }
-        template = jinja_environment.get_template('photo.html')
         lock.release()
-        self.response.out.write(template.render(template_values))
+
+        render_template(self, 'photo.html', template_values)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-
     	user = users.get_current_user()
-        albumID = []
-        albumTitle = []
-        albumPhotoNum = []
         if user is not None:
+            nickname = users.get_current_user().nickname()
             logInOut = users.create_logout_url(self.request.uri)
             greeting = "Please Enter Your Picasa Album Address:"
             gd_client = gdata.photos.service.PhotosService()
+            
+            # try delete the user's album in the DB
+            # pass if can't find.
+            try:
+                query = "Where owner = '%s'" % (nickname)
+                allAlbum = Album.gql(query)
+                db.delete(allAlbum)
+            except:
+                print "no album deleted for %s" % (nickname)
+                pass
 
+            # Grab user album list from Google Picasa and store in DB    
             try:
                 albumList = gd_client.GetUserFeed(user=user)
-
                 for album in albumList.entry:
-                    albumID.append(album.gphoto_id.text)
-                    albumRef[album.title.text] = album.gphoto_id.text
-                    albumTitle.append(album.title.text)
-                    albumPhotoNum.append(album.numphotos.text)
-                    albumPhoto[album.title.text] = album.numphotos.text
+                    newAlbum = Album()
+                    newAlbum.owner = nickname
+                    newAlbum.albumName = album.title.text
+                    newAlbum.albumID = album.gphoto_id.text
+                    newAlbum.albumPhoto = album.numphotos.text
+                    newAlbum.put()
             except:
+                print "%s album not added into DB" % (nickname)
                 pass
+                    # Grab all available albums for the user
+            nickname = users.get_current_user().nickname()
+            query = "Where owner = '%s'" % (nickname)
+            time.sleep(1)
+            allAlbum = Album.gql(query)
+            print query
+            print allAlbum.count()
+
+            template_values = {
+                    'user': user,
+                    'logInOut': logInOut,
+                    'greeting': greeting,
+                    'albumInfo': allAlbum,
+            }
+            return render_template(self, 'index.html', template_values)
+        
         else:
             logInOut = users.create_login_url(self.request.uri)
             greeting = "Please Login to continue."
-                                           
-        template_values = {
-        		'user': user,
-        		'logInOut': logInOut,
+
+            template_values = {
+                'user': user,
+                'logInOut': logInOut,
                 'greeting': greeting,
-                'albumInfo': zip(*(albumTitle, albumID, albumPhotoNum)),
-        }
-        
-        template = jinja_environment.get_template('index.html')
-        self.response.out.write(template.render(template_values))
+            }
+            return render_template(self, 'index.html', template_values)
+        return render_template(self, 'index.html', template_values)
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                                ('/Retrieve', Retrieve)],
+                                ('/Retrieve/', Retrieve)],
                                debug=True)
                                
